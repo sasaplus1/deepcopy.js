b44e48a29fcfba4131706d49cb3425a2c3c3045a
sasaplus1 <sasaplus1@gmail.com>
2013-05-26 13:15:45 +0900

    fixed comment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// deepcopy Copyright(c) 2013 sasa+1
-// https://github.com/sasaplus1/deepcopy
-// Released under the MIT License.
+/*!
+ * deepcopy Copyright(c) 2013 sasa+1
+ * https://github.com/sasaplus1/deepcopy
+ * Released under the MIT License.
+ */
 
 module.exports = require('./lib/deepcopy');

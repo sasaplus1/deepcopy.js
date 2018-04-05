29fa32e018a1dc1e38c1508e9bdcae507594cb9e
sasaplus1 <sasaplus1@gmail.com>
2018-04-06 02:27:57 +0900

    updated unit test

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,27 +1,47 @@
-import assert from 'assert';
-import deepcopy from '../../';
-
 describe('deepcopy', function() {
-  it('can copy primitive values', function() {
-    assert(deepcopy('') === '');
+  describe('types', function() {
+    it('can copy number', function() {
+      assert(deepcopy(0) === 0);
+      assert(deepcopy(Number.MAX_SAFE_INTEGER) === Number.MAX_SAFE_INTEGER);
+      assert(deepcopy(Number.MIN_SAFE_INTEGER) === Number.MIN_SAFE_INTEGER);
+      assert(deepcopy(Number.POSITIVE_INFINITY) === Number.POSITIVE_INFINITY);
+      assert(deepcopy(Number.NEGATIVE_INFINITY) === Number.NEGATIVE_INFINITY);
+      assert(Number.isNaN(deepcopy(NaN)));
+    });
 
-    assert(deepcopy(0) === 0);
-    assert(deepcopy(Number.MAX_SAFE_INTEGER) === Number.MAX_SAFE_INTEGER);
-    assert(deepcopy(Number.MIN_SAFE_INTEGER) === Number.MIN_SAFE_INTEGER);
-    assert(deepcopy(Number.POSITIVE_INFINITY) === Number.POSITIVE_INFINITY);
-    assert(deepcopy(Number.NEGATIVE_INFINITY) === Number.NEGATIVE_INFINITY);
-    assert(Number.isNaN(deepcopy(NaN)));
+    it('can copy Number', function() {
+      assert(deepcopy(new Number()) instanceof Number);
+    });
 
-    assert(deepcopy(true) === true);
-    assert(deepcopy(false) === false);
+    it('can copy string', function() {
+      assert(deepcopy('') === '');
+    });
 
-    assert(deepcopy(null) === null);
-    assert(deepcopy(undefined) === undefined);
-  });
+    it('can copy String', function() {
+      assert(deepcopy(new String()) instanceof String);
+    });
 
-  it('can copy symbol', function() {
-    const symbol = Symbol();
+    it('can copy boolean', function() {
+      assert(deepcopy(true) === true);
+      assert(deepcopy(false) === false);
+    });
 
-    assert(deepcopy(symbol) === symbol);
+    it('can copy Boolean', function() {
+      assert(deepcopy(new Boolean()) instanceof Boolean);
+    });
+
+    it('can copy null', function() {
+      assert(deepcopy(null) === null);
+    });
+
+    it('can copy undefined', function() {
+      assert(deepcopy(undefined) === undefined);
+    });
+
+    it('can copy symbol', function() {
+      const symbol = Symbol();
+
+      assert(deepcopy(symbol) === symbol);
+    });
   });
 });

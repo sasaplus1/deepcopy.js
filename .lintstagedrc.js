c5dc04fce766075cae661d8ee9b318a6f666ed87
sasaplus1 <sasaplus1@gmail.com>
2019-02-05 15:42:10 +0900

    added .huskyrc.js and .lintstagedrc.js

---
+++ .lintstagedrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  linters: {
+    '*.{js,mjs}': 'npx --no-install eslint',
+    '*.yml': [
+      'npx --no-install prettier --parser yaml --write',
+      'git diff --exit-code --quiet'
+    ],
+    '!(package|package-lock).json': [
+      'npx --no-install prettier --parser json-stringify --write',
+      'git diff --exit-code --quiet'
+    ],
+    'package.json': [
+      'npx fixpack',
+      'npx --no-install prettier --parser json-stringify --write',
+      'git diff --exit-code --quiet'
+    ]
+  }
+};

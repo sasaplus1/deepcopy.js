87a664cd9817f22460fcc827d07ea567bbbf5232
github-action-benchmark <github@users.noreply.github.com>
2020-08-21 20:03:49 +0000

    add Benchmark (benchmarkjs) benchmark result for 6861b9681715e2b88a27b86ae845eec81fb74965

---
+++ benchmark/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1598040229764,
+  "repoUrl": "https://github.com/sasaplus1/deepcopy.js",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sasaplus1@gmail.com",
+            "name": "sasaplus1",
+            "username": "sasaplus1"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6861b9681715e2b88a27b86ae845eec81fb74965",
+          "message": "Merge pull request #207 from sasaplus1/benchmark-with-gh-pages\n\nimprove benchmark workflow",
+          "timestamp": "2020-08-22T05:02:41+09:00",
+          "tree_id": "a88df5cf259e32087e232509943a475760a849d0",
+          "url": "https://github.com/sasaplus1/deepcopy.js/commit/6861b9681715e2b88a27b86ae845eec81fb74965"
+        },
+        "date": 1598040229357,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "deepcopy:fixture1",
+            "value": 23357,
+            "range": "±1.35%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          }
+        ]
+      }
+    ]
+  }
+}

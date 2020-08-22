a2b229b7c19f5a2587834053b2e5c8f1e3202f38
github-action-benchmark <github@users.noreply.github.com>
2020-08-22 16:21:16 +0000

    add Benchmark (benchmarkjs) benchmark result for cc1dd024e7ff712d9d3c95ade3bb01ad2e6dd058

--- benchmark/data.js
+++ benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1598040229764,
+  "lastUpdate": 1598113276651,
   "repoUrl": "https://github.com/sasaplus1/deepcopy.js",
   "entries": {
     "Benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.35%",
             "unit": "ops/sec",
             "extra": "87 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sasaplus1@gmail.com",
+            "name": "sasaplus1",
+            "username": "sasaplus1"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cc1dd024e7ff712d9d3c95ade3bb01ad2e6dd058",
+          "message": "Merge pull request #208 from sasaplus1/typescript\n\nmigrate to TypeScript",
+          "timestamp": "2020-08-23T01:20:32+09:00",
+          "tree_id": "79eacc0d0175dd74e48ceea5f675aba881636bd1",
+          "url": "https://github.com/sasaplus1/deepcopy.js/commit/cc1dd024e7ff712d9d3c95ade3bb01ad2e6dd058"
+        },
+        "date": 1598113275747,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "deepcopy:fixture1",
+            "value": 25810,
+            "range": "±1.85%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
           }
         ]
       }

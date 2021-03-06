1176c0a0258be088b8708051a374e217dc0fc24a
sasaplus1 <sasaplus1@gmail.com>
2021-03-06 20:17:04 +0900

    update benchmark

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,14 +1,16 @@
-#!/usr/bin/env node
-
 const Benchmark = require('benchmark');
 
 const { deepcopy } = require('../dist/cjs');
-const { fixture1 } = require('./fixtures');
+
+// $ curl -L -o fixture1.json https://api.github.com/repos/sasaplus1/deepcopy.js
+const fixture1 = require('./fixture1');
 
 const suite = new Benchmark.Suite();
 
+const env = process.env.ENVIRONMENT ? process.env.ENVIRONMENT + ':' : '';
+
 suite
-  .add('deepcopy:fixture1', function () {
+  .add(env + 'deepcopy:fixture1', function () {
     deepcopy(fixture1);
   })
   .on('cycle', function (event) {

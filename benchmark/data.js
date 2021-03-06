169c8003cf4d56fa158608eee8199b0b97519fd9
github-action-benchmark <github@users.noreply.github.com>
2021-03-06 18:33:32 +0000

    add Benchmark (benchmarkjs) benchmark result for 181a6dbd69e24d134f178b7d95f28d13e2519f69

--- benchmark/data.js
+++ benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1615055607349,
+  "lastUpdate": 1615055611997,
   "repoUrl": "https://github.com/sasaplus1/deepcopy.js",
   "entries": {
     "Benchmark": [
@@ -93,6 +93,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.54%",
             "unit": "ops/sec",
             "extra": "85 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sasaplus1@gmail.com",
+            "name": "sasaplus1",
+            "username": "sasaplus1"
+          },
+          "committer": {
+            "email": "sasaplus1@gmail.com",
+            "name": "sasaplus1",
+            "username": "sasaplus1"
+          },
+          "distinct": true,
+          "id": "181a6dbd69e24d134f178b7d95f28d13e2519f69",
+          "message": "remove eslint-plugin-prettier",
+          "timestamp": "2021-03-07T03:32:41+09:00",
+          "tree_id": "4748307fd9de8c75048dab9b8178b845778792e7",
+          "url": "https://github.com/sasaplus1/deepcopy.js/commit/181a6dbd69e24d134f178b7d95f28d13e2519f69"
+        },
+        "date": 1615055611509,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "node-14:deepcopy:fixture1",
+            "value": 23872,
+            "range": "±0.72%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
           }
         ]
       }
